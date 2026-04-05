f78d35ff328b29c982b93cde92df9ada93a62023
omer <omer>
2026-04-05 11:03:50 +0300

    first commit

---
+++ Assignment_1/part_1.py
@@ -0,0 +1,21 @@
+def initialize_parameters(layer_dims):
+    
+    
+def linear_forward(A, W, b):
+    
+def softmax(Z):
+    
+    
+def relu(Z):
+    
+
+def linear_activation_forward(A_prev, W, B, activation):
+    
+    
+def l_model_forward(X, parameters, use_batchnorm):
+    
+    
+def compute_cost(AL, Y):
+    
+    
+def apply_batchnorm(A):
