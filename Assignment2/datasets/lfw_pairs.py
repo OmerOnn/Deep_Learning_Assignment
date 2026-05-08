7f75816cc3a080a0dd8701e3155b54daaca07dd0
Yonisegall <Yonisegall@gmail.com>
2026-05-08 11:21:33 +0300

    Step 3

--- Assignment2/datasets/lfw_pairs.py
+++ Assignment2/datasets/lfw_pairs.py
@@ -4,27 +4,24 @@ from torch.utils.data import Dataset
 
 
 class LFWDataset(Dataset):
-    def __init__(self, pairs_file, lfw_root, transform=None):
+    def __init__(self, pairs_file, lfw_root, transform=None, return_paths=False):
         self.lfw_root = lfw_root
         self.transform = transform
+        self.return_paths = return_paths
         self.pairs = []
 
-        with open(pairs_file, 'r') as f:
+        with open(pairs_file, "r") as f:
             lines = f.readlines()
 
-        # שורה ראשונה = מספר identities (לא צריך בפועל)
         for line in lines[1:]:
             parts = line.strip().split()
 
             if len(parts) == 3:
-                # זוג חיובי
                 name = parts[0]
                 img1 = int(parts[1])
                 img2 = int(parts[2])
                 self.pairs.append((name, img1, name, img2, 1))
-
             elif len(parts) == 4:
-                # זוג שלילי
                 name1 = parts[0]
                 img1 = int(parts[1])
                 name2 = parts[2]
@@ -37,12 +34,8 @@ class LFWDataset(Dataset):
     def __getitem__(self, idx):
         name1, img1_idx, name2, img2_idx, label = self.pairs[idx]
 
-        img1_path = os.path.join(
-            self.lfw_root, name1, f"{name1}_{img1_idx:04d}.jpg"
-        )
-        img2_path = os.path.join(
-            self.lfw_root, name2, f"{name2}_{img2_idx:04d}.jpg"
-        )
+        img1_path = os.path.join(self.lfw_root, name1, f"{name1}_{img1_idx:04d}.jpg")
+        img2_path = os.path.join(self.lfw_root, name2, f"{name2}_{img2_idx:04d}.jpg")
 
         img1 = Image.open(img1_path).convert("RGB")
         img2 = Image.open(img2_path).convert("RGB")
@@ -50,5 +43,8 @@ class LFWDataset(Dataset):
         if self.transform:
             img1 = self.transform(img1)
             img2 = self.transform(img2)
+
+        if self.return_paths:
+            return img1, img2, label, img1_path, img2_path
 
         return img1, img2, label
